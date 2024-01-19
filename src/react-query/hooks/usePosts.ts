5a8f0de6d902966cd332e11d620830dda2ff26d3
remiodufuye <remi.odufuye@gmail.com>
2024-01-19 10:26:54 -0500

    Paginated Queries

--- src/react-query/hooks/usePosts.ts
+++ src/react-query/hooks/usePosts.ts
@@ -9,17 +9,23 @@ interface Post {
     userId: number;
   }
   
+interface PostQuery {
+  page:number;
+  pageSize:number
+}
 
-const usePosts = (userId:number | undefined) => useQuery<Post[],Error>({
-    queryKey: userId ? ['users',userId,'posts'] :['posts'],
+const usePosts = (query:PostQuery) => useQuery<Post[],Error>({
+    queryKey: ['posts',query],
     queryFn:() =>axios 
     .get("https://jsonplaceholder.typicode.com/posts",{
       params:{
-        userId
+        _start:(query.page - 1) * query.pageSize,
+        _limit:query.pageSize
       }
     })
     .then((res) => res.data),
-    staleTime: 1 * 60 * 1000
+    staleTime: 1 * 60 * 1000,
+    keepPreviousData:true
 });
 
 
