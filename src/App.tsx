07a4d50a2a08aaed1211cbb80887d3befe931b1e
remiodufuye <remi.odufuye@gmail.com>
2024-03-15 20:56:20 -0400

    Sharing State With React Context

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import { useReducer } from "react";
 import "./App.css";
-
-import LoginStatus from "./state-management/LoginStatus";
+import tasksReducer from "./state-management/reducers/tasksReducer";
+// import HomePage from "./routing/HomePage";
+import NavBar from "./state-management/NavBar";
+import HomePage from "./state-management/HomePage";
+import TasksContext from "./state-management/contexts/tasksContext";
 
 function App() {
-  return <LoginStatus />;
+  const [tasks, dispatch] = useReducer(tasksReducer, []);
+
+  return (
+    <TasksContext.Provider value={{ tasks, dispatch }}>
+      <NavBar />
+      <HomePage />
+    </TasksContext.Provider>
+  );
 }
 
 export default App;
