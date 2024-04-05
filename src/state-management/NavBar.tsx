3ba4befbf3802c027574bcfc10a24e980e75bc61
remiodufuye <remi.odufuye@gmail.com>
2024-04-05 19:31:56 -0400

    prevent uncessary renders with selectors

--- src/state-management/NavBar.tsx
+++ src/state-management/NavBar.tsx
@@ -5,7 +5,9 @@ import useCounterStore from "./counter/store";
 
 const NavBar = () => {
   const { tasks } = useContext(TasksContext);
-  const { counter } = useCounterStore();
+  const counter = useCounterStore((s) => s.counter);
+
+  console.log("Render NavBar");
 
   return (
     <nav className="navbar d-flex justify-content-between">
