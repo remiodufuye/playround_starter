c2f742e22266b74e1b80b73a6693f57522fd60fb
remiodufuye <remi.odufuye@gmail.com>
2024-04-05 08:21:12 -0400

    cleaned up dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import { useReducer } from "react";
 import "./App.css";
 import tasksReducer from "./state-management/reducers/tasksReducer";
-// import HomePage from "./routing/HomePage";
 import NavBar from "./state-management/NavBar";
 import HomePage from "./state-management/HomePage";
 import TasksContext from "./state-management/contexts/tasksContext";
-import authReducer from "./state-management/reducers/authreducer";
-import AuthContext from "./state-management/contexts/authContext";
 import AuthProvider from "./state-management/authProvider";
 
 function App() {
